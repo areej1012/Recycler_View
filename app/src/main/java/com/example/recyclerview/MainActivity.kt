7e8d7c167a329a72a35f1ab222a5a6567675fbc6
areej <areej101223@gmail.com>
2021-11-28 11:36:10 +0300

    Numbers Game App

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.recyclerview
 
+import android.content.DialogInterface
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.provider.CalendarContract
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.isDigitsOnly
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var inputUser : TextView
@@ -16,6 +22,8 @@ class MainActivity : AppCompatActivity() {
      var  items = ArrayList<String>()
     private lateinit var lymain : ConstraintLayout
     private lateinit var itemsAapter : RecyclerViewAdapter
+    private var guessed = 3
+    private var   randNum = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +32,8 @@ class MainActivity : AppCompatActivity() {
         submit = findViewById(R.id.submit)
         inputUser = findViewById(R.id.tvInputUser)
         lymain = findViewById(R.id.lyMain)
-
+        items = arrayListOf("","")
+        val randNum = Random.nextInt(10)
         //set Adapter
         itemsAapter =  RecyclerViewAdapter(items)
 
@@ -36,17 +45,68 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
     }
 
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putStringArrayList("array",items)
+        outState.putInt("guessed", guessed)
+        outState.putInt("num", randNum)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        guessed = savedInstanceState.getInt("guessed")
+        randNum = savedInstanceState.getInt("num")
+    }
     private fun updateinfo() {
+         randNum = Random.nextInt(10)
         val text = inputUser.text.toString()
-        if(text.isEmpty()){
-            Snackbar.make(lymain,"Please make sure to enter text", Snackbar.LENGTH_SHORT).show()
+        if(!text.isDigitsOnly()){
+            Snackbar.make(lymain,"Please make sure to enter number", Snackbar.LENGTH_SHORT).show()
         }else {
-            items.add(text)
+            if(text.equals(randNum)) {
+               inputUser.isEnabled = false
+                submit.isEnabled = false
+                showAlert("You win!! \nPlay again?")
+            }
+
+            else{
+                guessed--
+                items.add("You guessed $text")
+                items.add("You have $guessed guessed left")
+            }
+            if(guessed==0){
+                inputUser.isEnabled = false
+                submit.isEnabled = false
+                items.add("You lose - The correct answer was $randNum")
+                items.add("Game Over")
+                showAlert("You lose...\nThe correct answer was $randNum.\n\nPlay again?")
+            }
             inputUser.text = ""
             itemsAapter.notifyDataSetChanged()
         }
+    }
+
+
+    private fun showAlert(title: String) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(title)
+            .setCancelable(false)
+            .setPositiveButton("Yes", DialogInterface.OnClickListener {
+                    dia, id -> this.recreate()
+            })
+            // negative button text and action
+            .setNegativeButton("No", DialogInterface.OnClickListener {
+                    dia, id -> dia.cancel()
+            })
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Game Over")
+        // show alert dialog
+        alert.show()
     }
 }
